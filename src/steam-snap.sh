f713414b96dbfcdaba948126e343ca96bd0e66f5
ashuntu <ashton.nelson@canonical.com>
2026-09-17 15:11:56 -0500

    ci: Add shellcheck job

--- src/steam-snap.sh
+++ src/steam-snap.sh
@@ -7,15 +7,15 @@ export PATH=$SNAP/bin:$PATH
 # If Steam has not bootstrapped, check to see if there is a non-snap
 # Steam library we can adopt.
 STEAMCONFIG=$HOME/.local/share
-if [ ! -L $STEAMCONFIG/Steam/steamapps ]; then
-    mkdir -p $STEAMCONFIG/Steam/config
-    REALHOME=$(getent passwd $UID | cut -d ':' -f 6)
-    if [ -x $REALHOME/.local/share/Steam/steam.sh ]; then
-        ln -s $REALHOME/.local/share/Steam/steamapps $STEAMCONFIG/Steam
-        ln -s $REALHOME/.local/share/Steam/config/libraryfolders.vdf $STEAMCONFIG/Steam/config/
-    elif [ -x $REALHOME/.steam/debian-installation/steam.sh ]; then
-        ln -s $REALHOME/.steam/debian-installation/steamapps $STEAMCONFIG/Steam
-        ln -s $REALHOME/.steam/debian-installation/config/libraryfolders.vdf $STEAMCONFIG/Steam/config/
+if [ ! -L "$STEAMCONFIG"/Steam/steamapps ]; then
+    mkdir -p "$STEAMCONFIG"/Steam/config
+    REALHOME=$(getent passwd "$UID" | cut -d ':' -f 6)
+    if [ -x "$REALHOME"/.local/share/Steam/steam.sh ]; then
+        ln -s "$REALHOME"/.local/share/Steam/steamapps "$STEAMCONFIG"/Steam
+        ln -s "$REALHOME"/.local/share/Steam/config/libraryfolders.vdf "$STEAMCONFIG"/Steam/config/
+    elif [ -x "$REALHOME"/.steam/debian-installation/steam.sh ]; then
+        ln -s "$REALHOME"/.steam/debian-installation/steamapps "$STEAMCONFIG"/Steam
+        ln -s "$REALHOME"/.steam/debian-installation/config/libraryfolders.vdf "$STEAMCONFIG"/Steam/config/
     fi
 fi
 
